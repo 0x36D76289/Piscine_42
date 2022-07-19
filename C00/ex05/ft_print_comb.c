2c4113b15e38a32995b591991cd3acb7a79c0e75
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2022-07-19 22:06:35 +0200

    C00

---
+++ C00/ex05/ft_print_comb.c
@@ -0,0 +1,56 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfrancie <cfrancie@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 16:03:36 by cfrancie          #+#    #+#             */
+/*   Updated: 2022/07/08 10:29:30 by cfrancie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(int a, int b, int c)
+{
+	char	tmp;
+
+	tmp = a + 48;
+	write(1, &tmp, 1);
+	tmp = b + 48;
+	write(1, &tmp, 1);
+	tmp = c + 48;
+	write(1, &tmp, 1);
+}
+
+void	ft_print_comb(void)
+{
+	int	a;
+	int	b;
+	int	c;
+
+	a = 0;
+	while (a <= 7)
+	{
+		b = a + 1;
+		while (b <= 8)
+		{
+			c = b + 1;
+			while (c <= 9)
+			{
+				ft_print(a, b, c);
+				if (!(a == 7 && b == 8 && c == 9))
+					write(1, ", ", 2);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cfrancie <cfrancie@student.42.fr >         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/06 16:04:49 by cfrancie          #+#    #+#             */
+/*   Updated: 2022/07/08 12:06:19 by cfrancie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print(int a, int b)
+{
+	ft_putchar('0' + (a / 10));
+	ft_putchar('0' + (a % 10));
+	ft_putchar(' ');
+	ft_putchar('0' + (b / 10));
+	ft_putchar('0' + (b % 10));
+}
+
+void	ft_print_comb2(void)
+{
+	int	a;
+	int	b;
+
+	a = 0;
+	b = 0;
+	while (a <= 98)
+	{
+		b = a + 1;
+		while (b <= 99)
+		{
+			ft_print(a, b);
+			if (!(a == 98 && b == 99))
+			{
+				write(1, ", ", 2);
+			}
+			b++;
+		}
+		a++;
+	}
+}
