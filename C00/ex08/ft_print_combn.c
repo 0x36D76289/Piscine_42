1fb5a2a076823674a4b82ea09a0a801cfb963594
0x36D76289 <71098898+0x36D76289@users.noreply.github.com>
2022-07-21 15:37:01 +0200

    ft_print_combn

--- C00/ex08/ft_print_combn.c
+++ C00/ex08/ft_print_combn.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_print_combn.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cfrancie <cfrancie@student.42.fr >         +#+  +:+       +#+        */
+/*   By: cfrancie <cfrancie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 17:41:06 by cfrancie          #+#    #+#             */
-/*   Updated: 2022/07/08 10:17:38 by cfrancie         ###   ########.fr       */
+/*   Updated: 2022/07/21 14:29:35 by cfrancie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,58 +17,49 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int		ft_lenint(int n, int *nums)
+void	ft_putcombn(char *str)
 {
 	int	i;
-	int	tmp;
 
-	tmp = 0;
 	i = 0;
-
-	while (n != 0 || nums[i] != "\0")
+	while (str[i])
 	{
-		n /= 10;
+		ft_putchar(str[i]);
 		i++;
 	}
+}
 
+int	ft_lenstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
 	return (i);
 }
 
-void	ft_print(int *res, int n, int size)
+void	ft_print_combn_rec(char	*str, int i)
 {
-	int	i;
 	int	size;
 
-	i = 0;
-	while (res[i])
+	size = ft_lenstr(str);
+	if (str[i])
 	{
-		if (res[i] < 0)
-		{
-			while (size != 0)
-			{
-				ft_putchar('0');
-				size--;
-			}
-		}
+
 	}
 }
-
-void	ft_print_comb(int length)
-{
-	// affiche les combinaisons de nombres de 1 à 9
-	
-}
-
 
 void	ft_print_combn(int n)
 {
-	// verifie si n est entre 1 et 9
-	// si oui, affiche les combinaisons de nombres de 1 à 9
+	char	*res;
+	int		i;
 	
-	if (n < 1 || n > 9)
+	i = 0;
+	while (i < n)
 	{
-		return ;
+		res[i] = i;
+		i++;
 	}
 	
-
 }
